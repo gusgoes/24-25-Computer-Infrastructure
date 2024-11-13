0a08ef38670d65ec030adf4fe661d85aeb694cbc
Gustavo Fernandes <G00425776@atu.ie>
2024-11-13 20:20:05 +0000

    week 7

--- hello.sh
+++ hello.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# check if the user has provided a name
-if [ -z "$1" ]; then
+if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <name>"
     exit 1
 fi
 
-# greet the user
 echo "Hello, $1"
