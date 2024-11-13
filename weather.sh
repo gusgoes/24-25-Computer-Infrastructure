051a99a379e028382adeaeefd98ea88990a52046
Gustavo Fernandes <G00425776@atu.ie>
2024-11-13 19:37:36 +0000

    week 7

--- weather.sh
+++ weather.sh
@@ -1,3 +1,7 @@
-#: bin/bash
+#! bin/bash
 
+date
+echo "Downloading weather data"
 wget -O data/weather/`date +"%Y%m%d_%H%M%S"_athenry.json` https://prodapi.metweb.ie/observations/athenry/today
+echo "Weather data downloadded"
+date
