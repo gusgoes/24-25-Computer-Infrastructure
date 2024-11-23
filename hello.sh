9eae557455171d3604726a84653cacf1a8cbd41a
Gustavo Fernandes <G00425776@atu.ie>
2024-11-23 18:29:28 +0000

    Update

--- hello.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <name>"
-    exit 1
-fi
-
-echo "Hello, $1"
--- weather.sh
+++ weather.sh
@@ -3,5 +3,5 @@
 date
 echo "Downloading weather data"
 wget -O data/weather/`date +"%Y%m%d_%H%M%S"_athenry.json` https://prodapi.metweb.ie/observations/athenry/today
-echo "Weather data downloadded"
+echo "Weather data downloaded"
 date
